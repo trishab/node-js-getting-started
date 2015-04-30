2ea335bb616e6edce0cdab2cc91b3c07a77c2480
Trisha Black <tblack818@gmail.com>
2015-04-30 13:55:32 +0000

    updating index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+var app = express.createServer(express.logger());
+var fs. = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World4!');
-});
+  var string_var = fs.readFileSync('index.html').toString();
+  response.send(string_var);
+}
+
+app.set('port',(process,env.PORT || 500));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
